a996a664fe990f09610eceda1619f961af89c331
adoniiaz <91956393+adoniiaz@users.noreply.github.com>
2022-10-14 21:34:35 +0300

    Delete project.Tbl.Teacher.sql

--- project.Tbl.Teacher.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [project].[Tbl.Teacher] (
-    [TeacherID]     INT           IDENTITY (1, 1) NOT NULL,
-    [FirstName]     VARCHAR (100) NOT NULL,
-    [LastName]      VARCHAR (100) NOT NULL,
-    [ClassesTaught] VARCHAR (100) NOT NULL,
-    [Passd]         VARCHAR (100) NOT NULL,
-    CONSTRAINT [Pk_Tbl.Teacher_TeacherID] PRIMARY KEY CLUSTERED ([TeacherID] ASC)
-);
-
