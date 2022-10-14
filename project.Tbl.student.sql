22fd3dee83e862e44f3ab018bb170b41040f3efa
amuyif <100996038+amuyif@users.noreply.github.com>
2022-10-14 21:32:30 +0300

    Add files via upload

---
+++ project.Tbl.student.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [project].[Tbl.student] (
+    [StudentID]   INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName]   VARCHAR (100) NOT NULL,
+    [LastName]    VARCHAR (100) NOT NULL,
+    [BatchNumber] VARCHAR (100) NOT NULL,
+    [StudentCode] VARCHAR (100) NOT NULL,
+    [passd]       VARCHAR (100) NOT NULL,
+    CONSTRAINT [Pk_Tbl.student_StudentID] PRIMARY KEY CLUSTERED ([StudentID] ASC)
+);
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'This table consists of all the data input by the student about themselves', @level0type = N'SCHEMA', @level0name = N'project', @level1type = N'TABLE', @level1name = N'Tbl.student';
+
