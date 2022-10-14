8b07efed487a59839be5727a2c5b88ae772b0fa1
EliasMeaza <107608460+EliasMeaza@users.noreply.github.com>
2022-10-14 21:37:17 +0300

    Add files via upload

---
+++ project.Tbl.ClassAttendance.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [project].[Tbl.ClassAttendance] (
+    [AttendanceID]   INT           IDENTITY (1, 1) NOT NULL,
+    [StudentName]    VARCHAR (100) NOT NULL,
+    [BatchNumber]    VARCHAR (100) NOT NULL,
+    [ClassName]      VARCHAR (100) NOT NULL,
+    [TeacherName]    VARCHAR (100) NOT NULL,
+    [AttendanceData] VARCHAR (100) NOT NULL,
+    [DatenTime]      DATE          NOT NULL,
+    CONSTRAINT [Pk_Tbl.ClassAttendance_AttendanceID] PRIMARY KEY CLUSTERED ([AttendanceID] ASC)
+);
+
---
+++ project.Tbl.Year.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [project].[Tbl.Year] (
+    [YearID]       INT           IDENTITY (1, 1) NOT NULL,
+    [BatchNumber]  VARCHAR (100) NOT NULL,
+    [BarchYear]    VARCHAR (100) NOT NULL,
+    [BatchClasses] VARCHAR (100) NOT NULL,
+    CONSTRAINT [Pk_Tbl.Year_YearID] PRIMARY KEY CLUSTERED ([YearID] ASC)
+);
+
