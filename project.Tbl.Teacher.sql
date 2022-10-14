899f825d363ed7a40444ba348a9979e3664f7a83
adoniiaz <91956393+adoniiaz@users.noreply.github.com>
2022-10-14 21:34:08 +0300

    Add files via upload

---
+++ project.Tbl.Teacher.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [project].[Tbl.Teacher] (
+    [TeacherID]     INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName]     VARCHAR (100) NOT NULL,
+    [LastName]      VARCHAR (100) NOT NULL,
+    [ClassesTaught] VARCHAR (100) NOT NULL,
+    [Passd]         VARCHAR (100) NOT NULL,
+    CONSTRAINT [Pk_Tbl.Teacher_TeacherID] PRIMARY KEY CLUSTERED ([TeacherID] ASC)
+);
+
