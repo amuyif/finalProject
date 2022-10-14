db164f5683972005707abf630096b525e19ba0c8
amuyif <100996038+amuyif@users.noreply.github.com>
2022-10-14 21:33:26 +0300

    Add files via upload

---
+++ project.Tbl.Class.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [project].[Tbl.Class] (
+    [ClassID]       INT           IDENTITY (1, 1) NOT NULL,
+    [ClassName]     VARCHAR (100) NOT NULL,
+    [ClassBatch]    VARCHAR (100) NOT NULL,
+    [StudentNumber] INT           NOT NULL,
+    [ClassTeacher]  VARCHAR (100) NOT NULL,
+    CONSTRAINT [Pk_Tbl.Class_ClassID] PRIMARY KEY CLUSTERED ([ClassID] ASC)
+);
+
