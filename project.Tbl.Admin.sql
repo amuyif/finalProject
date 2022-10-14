5d40f0e0010f7c9703dcbecce622c8663acd2eea
adoniiaz <91956393+adoniiaz@users.noreply.github.com>
2022-10-14 21:36:10 +0300

    Add files via upload

---
+++ project.Tbl.Admin.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [project].[Tbl.Admin] (
+    [AdminID]        INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName]      VARCHAR (100) NOT NULL,
+    [LastName]       VARCHAR (100) NOT NULL,
+    [Passd]          VARCHAR (100) NOT NULL,
+    [ManagedClasses] VARCHAR (100) NOT NULL,
+    CONSTRAINT [Pk_Tbl.Admin_AdminID] PRIMARY KEY CLUSTERED ([AdminID] ASC)
+);
+
